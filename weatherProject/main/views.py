8bc8c12ce8cb4d254c5a03627c2036eea13f0b5a
amir bekhit <bekdevelopment@gmail.com>
2020-12-20 15:39:49 +0100

    initial commit

---
+++ weatherProject/main/views.py
@@ -0,0 +1,31 @@
+import urllib.request
+import json
+from django.shortcuts import render
+
+
+def index(request):
+
+    if request.method == 'POST':
+        city = request.POST['city']
+
+        source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+                                        city + '&units=metric&appid=<YOUR API KEY>').read()
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '
+            + str(list_of_data['coord']['lat']),
+
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+        }
+        print(data)
+    else:
+        data = {}
+
+    return render(request, "main/index.html", data)
